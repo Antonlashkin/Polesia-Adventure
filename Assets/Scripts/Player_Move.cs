30941a6fd17af6fd3b8f8be6ea2744b90f8ba147
Антон Лашкин <antonlaskin38@gmail.com>
2023-11-21 20:52:30 +0300

    Added moving function to character

---
+++ Assets/Scripts/Player_Move.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    private Rigidbody2D rigidBody;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        Vector2 movement = new Vector2 (deltaX, deltaY);
+        movement = Vector2.ClampMagnitude(movement, speed);
+        //rigidBody.velocity = new Vector2(transform.position.x + movement.x, transform.position.y + movement.y);
+        //Debug.Log(transform.position);
+        //rigidBody.MovePosition(position);
+        transform.Translate(movement);
+    }
+}
