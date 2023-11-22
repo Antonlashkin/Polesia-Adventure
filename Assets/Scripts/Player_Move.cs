8e57bce8f1feccb0e908810a67d8c00ec1954fde
Антон Лашкин <antonlaskin38@gmail.com>
2023-11-22 22:26:49 +0300

    Added collision

--- Assets/Scripts/Player_Move.cs
+++ Assets/Scripts/Player_Move.cs
@@ -4,6 +4,7 @@ public class Player_Move : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
     private Rigidbody2D rigidBody;
+    [SerializeField] private LayerMask solidObjectsLayer;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
@@ -15,9 +16,12 @@ public class Player_Move : MonoBehaviour
         float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
         Vector2 movement = new Vector2 (deltaX, deltaY);
         movement = Vector2.ClampMagnitude(movement, speed);
-        //rigidBody.velocity = new Vector2(transform.position.x + movement.x, transform.position.y + movement.y);
+        Vector2 point = new Vector2(transform.position.x + movement.x, transform.position.y + movement.y);
         //Debug.Log(transform.position);
         //rigidBody.MovePosition(position);
-        transform.Translate(movement);
+        if (Physics2D.OverlapBox(point, new Vector2(0.95f, 1.9f), 0f, solidObjectsLayer) == null)
+        {
+            transform.Translate(movement);
+        }
     }
 }
