b60804b77abee1f3314a87da63a518904d2a2fa7
Antonlashkin <antonlaskin38@gmail.com>
2024-07-15 18:29:16 +0300

    Fix collisions and movements in level scene

--- Assets/Scripts/Player/CameraMove.cs
+++ Assets/Scripts/Player/CameraMove.cs
@@ -10,15 +10,16 @@ public class CameraMove : MonoBehaviour
         {
             Cam.transform.position = new Vector3(7, Cam.transform.position.y, Cam.transform.position.z);
         }
-         else if (Player.transform.position.x < -7)
+        else if (Player.transform.position.x < -7)
         {
             Cam.transform.position = new Vector3(-7, Cam.transform.position.y, Cam.transform.position.z);
         }
+        
         if (Player.transform.position.y > 7)
         {
             Cam.transform.position = new Vector3(Cam.transform.position.x, 7, Cam.transform.position.z);
         }
-        if (Player.transform.position.y < -7)
+        else if (Player.transform.position.y < -7)
         {
             Cam.transform.position = new Vector3(Cam.transform.position.x, -7, Cam.transform.position.z);
         }
--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -17,30 +17,36 @@ public class PlayerMove : MonoBehaviour
     {
         float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
         float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        
         Vector2 movement = new Vector2 (deltaX, deltaY);
         movement = Vector2.ClampMagnitude(movement, speed);
-        Vector2 point = new Vector2(transform.position.x + movement.x, transform.position.y + movement.y);
-        //Debug.Log(transform.position);
-        //rigidBody.MovePosition(position);
-        if (Physics2D.OverlapBox(point, new Vector2(0.95f, 1.9f), 0f, solidObjectsLayer) == null)
+        
+        //Vector2 point = new Vector2(transform.position.x + movement.x, transform.position.y + movement.y);
+        
+        // if (Physics2D.OverlapBox(point, new Vector2(0.95f, 1.9f), 0f, solidObjectsLayer) == null)
+        // {
+        // }
+        
+        if (transform.position.x > xMapSize && movement.x > 0)
         {
-            if (transform.position.x > xMapSize && movement.x > 0)
-            {
-                movement.x = 0;
-            }
-            else if (transform.position.x < -xMapSize && movement.x < 0)
-            {
-                movement.x = 0;
-            }
-            if (transform.position.y > yMapSize && movement.y > 0)
-            {
-                movement.y = 0;
-            }
-            else if (transform.position.y < -yMapSize && movement.y < 0)
-            {
-                movement.y = 0;
-            }
-            transform.Translate(movement);
+            movement.x = 0;
         }
+        else if (transform.position.x < -xMapSize && movement.x < 0)
+        {
+            movement.x = 0;
+        }
+            
+        if (transform.position.y > yMapSize && movement.y > 0)
+        {
+            movement.y = 0;
+        }
+        else if (transform.position.y < -yMapSize && movement.y < 0)
+        {
+            movement.y = 0;
+        }
+        
+        rigidBody.velocity = movement;
+        Debug.Log(rigidBody.velocity);
+        //transform.Translate(movement);
     }
 }
